3e64fc1c9a63aea26eeb851edb979032c89ab0b5
sanober riaz <sanober@sanober.me>
2018-01-07 05:24:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,44 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ cart
+ //var item = [];
+ var price = Math.floor(Math.random(101) * (100 - 1)) + 1
+ var items = {[item]:price}
+ //Object.assign(myc, {[name]: price});
+ cart.push(items)
+ //cart
+ console.log(`${item} has been added to your cart.`)
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  cart;
+  let l = cart.length;
+  var s = "In your cart, you have "
+while(!cart.lenth){
+  switch (l) {
+
+    case (l === 1):
+      return s += `${cart[0]} at $${cart[0][0]}.`;
+      break;
+
+    case (l >1 ):
+    var c = 1;
+
+      for (var i = 0; i < cart.length; i++ ,  c< l-1) {
+        s += `${cart[i]} at $${cart[i][i]},`
+        c++;
+      }
+      return s += ` and ${cart[l-1]} at $${cart[l-1][l-1]}.`;
+
+      break;
+
+  }
+
+}
+return "Your shoppping cart is empty.";
 }
 
 function total() {
