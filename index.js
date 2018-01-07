dcac0c508c3034359b9dbd213f7097723aa0401b
sanober riaz <sanober@sanober.me>
2018-01-07 05:52:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,10 +28,32 @@ function viewCart() {
   var l = cart.length;
   var s = "In your cart, you have "
 if (!cart.length) {
-  console.log( "Your shopping cart is empty.")
-}else if (cart.length === 1) {
-  console.log(s + `${cart[0]} at $${cart[0][0]}.`);
+  console.log( "Your shopping cart is empty.");
 }
+const l = cart.length
+let itemsAndPrices = [];
+
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+
+    itemsAndPrices.push(`${item} at $${price}`);
+  }
+
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and ");
+      break;
+    default:
+      itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+      itemsAndPrices = itemsAndPrices.join(", ");
+  }
+
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
+
 
 }
 
